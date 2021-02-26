1595478d5f560811e5fd920996e657b08c79415c
Ezequiel Fonseca <ezequiel_fs13@hotmail.com>
2021-02-26 20:14:58 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+export interface Props {
+  
+}
+
+const App: React.FC<Props> = (props) => (
+  <Text> Hello World </Text>
+);
+
+export default App;
