850c21a14ac1c32f464105dbafb6b23b1f9b4f5e
Ezequiel Fonseca <ezequiel_fs13@hotmail.com>
2021-03-03 01:21:09 -0300

    added assets, constants, and environment variables

---
+++ src/constants/index.tsx
@@ -0,0 +1,6 @@
+import { IMAGES } from "./images";
+import { COLORS, SIZES, FONTS } from "./theme";
+import GOOGLE_API_KEY from "./maps";
+
+export { IMAGES, COLORS, SIZES, FONTS, GOOGLE_API_KEY };
+
---
+++ src/constants/maps.tsx
@@ -0,0 +1,3 @@
+import {GOOGLE_API_KEY} from '@env';
+
+export default GOOGLE_API_KEY;
